d36f54271320e59a0c582f44c87f9fa19e19821e
kky107102 <kky107102@ajou.ac.kr>
2024-02-27 13:21:33 +0900

    similarity transform 후 rbf 여전히 끝이 늘어지는 현상

--- mediapipe/examples/desktop/face_deformation_main.cc
+++ mediapipe/examples/desktop/face_deformation_main.cc
@@ -89,17 +89,11 @@ absl::Status RunMPPGraph(Mat img, mediapipe::NormalizedLandmarkList& output_land
   return graph.WaitUntilDone();
 }     
 
-float thinPlateSpline(const Point2f& p1, const Point2f& p2) {
-  auto d = sqrt(pow(p1.x-p2.x, 2) + pow(p1.y-p2.y, 2));
-  if (d == 0) return 1;
-  return d*d*log(d);
-}
-
 Mat getLinearCoeff(const std::vector<Point2f>& src_landmarks, const std::vector<Point2f>& model_landmarks){
   int lmsize = src_landmarks.size();
   Mat modelPoints(lmsize, 2, CV_32F);
   Mat srcPoints(lmsize, 3, CV_32F);
-  Mat coeff(3, 2, CV_32F);
+  Mat coeff(2, 3, CV_32F);
 
   for (int i = 0; i < lmsize; i++){
     modelPoints.at<float>(i,0) = model_landmarks[i].x;
@@ -110,24 +104,27 @@ Mat getLinearCoeff(const std::vector<Point2f>& src_landmarks, const std::vector<
   }
   
   solve(srcPoints, modelPoints, coeff, DECOMP_SVD);
-  std::cout << coeff;
   return coeff;
 }
 
-std::tuple<Mat,Mat> SLR(const Mat& srcImg, const Mat& coeff){
-  Mat map_x = Mat::zeros(srcImg.size(), CV_32F);
-  Mat map_y = Mat::zeros(srcImg.size(), CV_32F);
+std::tuple<Mat,Mat> SLR(const Mat& dstImg, const Mat& coeff){
+  Mat map_x = Mat::zeros(dstImg.size(), CV_32F);
+  Mat map_y = Mat::zeros(dstImg.size(), CV_32F);
   
-  for (int row = 0; row < srcImg.rows; row++){
-    for (int col = 0; col < srcImg.cols; col++){
+  for (int row = 0; row < dstImg.rows; row++){
+    for (int col = 0; col < dstImg.cols; col++){
       map_x.at<float>(row,col) += (col * coeff.at<float>(0,0) + row * coeff.at<float>(1,0) + coeff.at<float>(2,0)); //x
       map_y.at<float>(row,col) += (col * coeff.at<float>(0,1) + row * coeff.at<float>(1,1) + coeff.at<float>(2,1)); //y
     }
   }
-
   return std::make_tuple(map_x, map_y);
 }
 
+float thinPlateSpline(const Point2f& p1, const Point2f& p2) {
+  auto d = sqrt(pow(p1.x-p2.x, 2) + pow(p1.y-p2.y, 2));
+  if (d == 0) return 1;
+  return d*d*log(d);
+}
 
 std::tuple<Mat,Mat> getRBFWeight(const std::vector<Point2f>& src_landmarks, const std::vector<Point2f>& model_landmarks) {
   int lmsize = src_landmarks.size();
@@ -157,27 +154,11 @@ std::tuple<Mat,Mat> RBF(const std::vector<Point2f>& src_landmarks, const Mat& sr
   for (int row = 0; row < srcImg.rows; row++){
     for (int col = 0; col < srcImg.cols; col++){
       for (int k = 0; k < src_landmarks.size(); k++){
-        map_x.at<float>(row,col) += std::get<0>(weight).at<float>(k)*thinPlateSpline(src_landmarks[k], Point2f(row,col)); 
-        map_y.at<float>(row,col) += std::get<1>(weight).at<float>(k)*thinPlateSpline(src_landmarks[k], Point2f(col,row));
-      }
-      map_x.at<float>(row,col) += col;
-      map_y.at<float>(row,col) += row;
-    }
-  }
-  return std::make_tuple(map_x, map_y);
-}
-
-std::tuple<Mat,Mat> RBF_SLR(const std::vector<Point2f>& src_landmarks, const Mat& srcImg, const std::tuple<Mat,Mat>& weight, const std::tuple<Mat,Mat>& coeff){
-  Mat map_x = Mat::zeros(srcImg.size(), CV_32F);
-  Mat map_y = Mat::zeros(srcImg.size(), CV_32F);
-  for (int row = 0; row < srcImg.rows; row++){
-    for (int col = 0; col < srcImg.cols; col++){
-      for (int k = 0; k < src_landmarks.size(); k++){
         map_x.at<float>(row,col) -= std::get<0>(weight).at<float>(k)*thinPlateSpline(src_landmarks[k], Point2f(row,col)); 
         map_y.at<float>(row,col) -= std::get<1>(weight).at<float>(k)*thinPlateSpline(src_landmarks[k], Point2f(col,row));
       }
-      map_x.at<float>(row,col) += col * std::get<0>(coeff).at<float>(0) + std::get<0>(coeff).at<float>(1);
-      map_y.at<float>(row,col) += row * std::get<1>(coeff).at<float>(0) + std::get<0>(coeff).at<float>(1);
+      map_x.at<float>(row,col) += col;
+      map_y.at<float>(row,col) += row;
     }
   }
   return std::make_tuple(map_x, map_y);
@@ -187,49 +168,53 @@ int main(int argc, char** argv) {
   google::InitGoogleLogging(argv[0]);
   absl::ParseCommandLine(argc, argv);
 
-  Mat myInputImg = imread("C:/Users/yeon/mediapipe_repo/mediapipe/mediapipe/examples/desktop/jehoon.jpg");
-  Mat myInputImg2 = imread("C:/Users/yeon/mediapipe_repo/mediapipe/mediapipe/examples/desktop/ms.jpg");
+  Mat srcImg = imread("C:/Users/yeon/mediapipe_repo/mediapipe/mediapipe/examples/desktop/ms.jpg");
+  Mat modelImg = imread("C:/Users/yeon/mediapipe_repo/mediapipe/mediapipe/examples/desktop/jehoon.jpg");
   mediapipe::NormalizedLandmarkList landmarks;
   mediapipe::NormalizedLandmarkList landmarks2;
-  RunMPPGraph(myInputImg, landmarks);
-  RunMPPGraph(myInputImg2, landmarks2);
+  RunMPPGraph(srcImg, landmarks);
+  RunMPPGraph(modelImg, landmarks2);
   std::vector<Point2f> p1;
   std::vector<Point2f> p2;
 
   for (int i = 0; i < landmarks.landmark_size(); i++){
-    p1.push_back(Point2f((float)landmarks.landmark(i).x()*myInputImg.cols,(float)landmarks.landmark(i).y()*myInputImg.rows));
-    p2.push_back(Point2f((float)landmarks2.landmark(i).x()*myInputImg2.cols,(float)landmarks2.landmark(i).y()*myInputImg2.rows));
+    p1.push_back(Point2f((float)landmarks.landmark(i).x()*srcImg.cols,(float)landmarks.landmark(i).y()*srcImg.rows));
+    p2.push_back(Point2f((float)landmarks2.landmark(i).x()*modelImg.cols,(float)landmarks2.landmark(i).y()*modelImg.rows));
     // 첫 번째 이미지에 랜드마크 빨간 점으로 표시
-    //circle(myInputImg, Point(landmarks.landmark(i).x()*myInputImg.cols, landmarks.landmark(i).y()*myInputImg.rows), 1, Scalar(0, 0, 255), - 1);
+    //circle(srcImg, Point(landmarks.landmark(i).x()*srcImg.cols, landmarks.landmark(i).y()*srcImg.rows), 1, Scalar(0, 0, 255), - 1);
     // 두 번째 이미지에 랜드마크 파란 점으로 표시
-    circle(myInputImg2, Point(landmarks2.landmark(i).x()*myInputImg2.cols, landmarks2.landmark(i).y()*myInputImg2.rows), 1, Scalar(255, 0, 0), - 1);
+    circle(modelImg, Point(landmarks2.landmark(i).x()*modelImg.cols, landmarks2.landmark(i).y()*modelImg.rows), 1, Scalar(255, 0, 0), - 1);
   }
-  Mat H = findHomography(p1, p2);
-  Mat imgwarp;
-  warpPerspective(myInputImg, imgwarp, H, Size(myInputImg.cols*1.5 , myInputImg.rows *1.5));
-  std::vector<Point2f> pointwarp;
-  perspectiveTransform(p1, pointwarp, H); 
+//   Mat H = findHomography(p1, p2);
+//   Mat imgwarp;
+//   warpPerspective(srcImg, imgwarp, H, Size(srcImg.cols*1.5 , srcImg.rows *1.5));
+//   std::vector<Point2f> pointwarp;
+//   perspectiveTransform(p1, pointwarp, H); 
+//   for (int i = 0; i < landmarks.landmark_size(); i++){
+//     // 두 번째 이미지에 첫번째 랜드마크 빨간 점으로 표시
+//     circle(modelImg, pointwarp[i], 1, Scalar(0, 0, 225), - 1);
+//   }
+  auto coeff = getLinearCoeff(p2, p1); //?
+  auto [map_x, map_y] = SLR(modelImg, coeff);
+  Mat similarityImg(srcImg.size() , srcImg.type());
+  remap(srcImg, similarityImg, map_x, map_y, INTER_LINEAR, BORDER_CONSTANT, Scalar(0,0,0));
+
+  mediapipe::NormalizedLandmarkList landmarks3;
+  RunMPPGraph(similarityImg, landmarks3);
+  std::vector<Point2f> p3;
   for (int i = 0; i < landmarks.landmark_size(); i++){
-    // 두 번째 이미지에 첫번째 랜드마크 빨간 점으로 표시
-    circle(myInputImg2, pointwarp[i], 1, Scalar(0, 0, 225), - 1);
+    p3.push_back(Point2f((float)landmarks3.landmark(i).x()*similarityImg.cols,(float)landmarks3.landmark(i).y()*similarityImg.rows));
+    circle(modelImg, p3[i], 1, Scalar(0, 0, 225), - 1);
   }
-  auto coeff = getLinearCoeff(p2, p1);
-  //auto weight = getRBFWeight(pointwarp, p2);
-  auto [map_x, map_y] = SLR(myInputImg, coeff);
-  //auto [map_x, map_y] = RBF(p1, myInputImg, weight);
-  //auto [map_x, map_y] = RBF_SLR(p1, imgwarp, weight, coeff);
-  
-  Mat dst(Size(myInputImg.cols*2 , myInputImg.rows *2) , myInputImg.type());
-  remap(myInputImg, dst, map_x, map_y, INTER_LINEAR, BORDER_CONSTANT, Scalar(0,0,0));
-  //remap(myInputImg, dst, map_x, map_y, INTER_LINEAR, BORDER_CONSTANT, Scalar(0,0,0));
-  //auto weight = getRBFWeight(p1, p2);
-  //auto [map_x, map_y] = RBF(p1, dst, weight);
-  //remap(dst, dst, map_x, map_y, INTER_LINEAR, BORDER_CONSTANT, Scalar(0,0,0));
 
+  Mat dst(srcImg.size() , srcImg.type());  
+  auto weight = getRBFWeight(p3, p2);
+  auto [map_x1, map_y2] = RBF(p3, similarityImg, weight);
+  remap(similarityImg, dst, map_x1, map_y2, INTER_LINEAR, BORDER_CONSTANT, Scalar(0,0,0));
 
-  imshow("img0", myInputImg);
-  imshow("img1", imgwarp);
-  imshow("img2", myInputImg2);
+  imshow("img0", similarityImg);
+  //imshow("img1", imgwarp);
+  imshow("img2", modelImg);
   imshow("img3", dst);
   waitKey();
 
